c82ef325d0e05043add6884bdda8335fc21dbde4
Saraivinha1703 <carlos.saraiva.neto@gmail.com>
2024-03-14 20:22:42 +0000

    📦 NEW: Adding shadcn ui

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+import { BaseThemeOptions } from "@/types/theme-options"
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
